d5d3caa4f0948cf7afbe2963c1b65c1914cde172
beeper <zhangxue233@outlook.com>
2023-03-22 07:46:45 +0000

    Create tweet page, add display of tweet content

---
+++ components/tweetContent/tweetContent.js
@@ -0,0 +1,25 @@
+import Layout from "../Layout";
+import Navigate from "../Navigate";
+import Cover from "../Cover";
+import TweetText from "./tweetText";
+import {useEffect, useState} from "react";
+
+export default function TweetContent(){
+    return(
+        <div className="w-full flex flex-col justify-center items-center">
+            <div className="w-full justify-start items-stretch pb-10">
+            <div className="px-5 pt-2">
+                <Navigate title="Tweet"/>
+            </div>
+            
+                <div className="px-7 ">
+                    <TweetText content="testing testing"/>
+                </div>
+            
+            </div>
+            <div>
+                reply (unfinished)
+            </div>
+        </div>
+    );
+}
