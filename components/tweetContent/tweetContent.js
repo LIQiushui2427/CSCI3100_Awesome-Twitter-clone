2eb143721a5c4a3daac13235f44415bab8caad05
beeper <zhangxue233@outlook.com>
2023-03-24 07:50:47 +0000

    Added commentText, modified commentBox, tweetText, tweetContent

--- components/tweetContent/tweetContent.js
+++ components/tweetContent/tweetContent.js
@@ -3,6 +3,7 @@ import Navigate from "../Navigate";
 import Cover from "../Cover";
 import TweetText from "./tweetText";
 import CommentBox from "./commentBox";
+import CommentText from "./commentText";
 
 export default function TweetContent(){
     return(
@@ -12,14 +13,23 @@ export default function TweetContent(){
                     <Navigate title="Tweet"/>
                 </div>
                 <div>
-                    <TweetText content="testing testing"/>
+                    <TweetText content="testing testing" time="Mar 23" />
                 </div>
                 <div>
                     <CommentBox />
                 </div>
-            </div>
-            <div>
-                reply (unfinished)
+                <div>
+                    <CommentText />
+                </div>
+                <div>
+                    <CommentText content="test1" nopic="true" />
+                </div>
+                <div>
+                    <CommentText authorname="John Doe" authorid="johndoe" avatarpic="https://picsum.photos/id/1005/40/40" content="test2" picture="https://pbs.twimg.com/media/EKJt7R6XYAEzdvt.png" />
+                </div>
+                <div>
+                    <CommentText authorname="beeper" authorid="c_beeper" content="reply (unfinished)" avatarpic="https://1.gravatar.com/avatar/69119e7cc6b6fed2cbc852051081eda0" picture="https://ksbeeper.files.wordpress.com/2020/11/e5b18fe5b995e688aae59bbe-2021-02-23-163818.gif" />
+                </div>
             </div>
         </div>
     );
