52bb63fdae468d8085228c3f5c6e82157a437b71
ZHANG, Xue <1155160250@link.cuhk.edu.hk>
2023-03-28 10:45:26 +0800

    modified structure

--- components/Cover.js
+++
@@ -1,7 +0,0 @@
-import EditableImage from "./EditableImage";
-
-export default function Cover({src,onChange,editable}) {
-  return (
-    <EditableImage type={'cover'} src={src} onChange={onChange} editable={editable} className={'h-48'} />
-  );
-}
