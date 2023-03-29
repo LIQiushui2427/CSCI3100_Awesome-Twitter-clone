9abf322ce7f490a3aee1fcde50817bfe56e38019
ZHANG, Xue <1155160250@link.cuhk.edu.hk>
2023-03-29 23:04:52 +0800

    register and login

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 export default {
-    JWT_SECRET: "kGOgh3Hlns7Ao+U1LniKixYRErv5ju2pWwQyVBRxKDk=",
-    EMAIL : 'a1214110899@gmail.com',
-    PASSWORD : 'etmbaadrdtaghdga'
+    JWT_SECRET : "kGOgh3Hlns7Ao+U1LniKixYRErv5ju2pWwQyVBRxKDk=",
+    EMAIL: "a1214110899@gmail.com", // testing email & password
+    PASSWORD : "etmbaadrdtaghdga",
+    ATLAS_URI: "mongodb+srv://admin:qrVdPoZtL678wDEp@3100twitter.5aozu8k.mongodb.net/?retryWrites=true&w=majority"
 }
-
