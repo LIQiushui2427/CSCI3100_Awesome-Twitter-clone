1931aca12a19ed228426fd5c9e02b3d4d5acdcec
LiQiushui <83109207+LIQiushui2427@users.noreply.github.com>
2023-03-29 23:29:36 +0800

    modified:   client/package.json 	renamed:    client/src/_app.js -> client/pages/_app.js 	renamed:    client/src/_document.js -> client/pages/_document.js 	renamed:    client/src/api/hello.js -> client/pages/api/hello.js 	renamed:    client/src/index.js -> client/pages/index.js 	renamed:    client/src/login.js -> client/pages/login.js 	renamed:    client/src/messages.js -> client/pages/messages.js 	renamed:    client/src/profile.js -> client/pages/profile.js 	renamed:    client/src/search.js -> client/pages/search.js 	renamed:    client/src/signup.js -> client/pages/signup.js 	renamed:    client/src/tweet.js -> client/pages/tweet.js 	renamed:    server/app.js -> server/server.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-
-
 import LeftPane from '../components/leftPane/leftPane';
 import RightPane from '../components/rightPane/rightPane';
 import Home from '../components/home/home';
+import App from "./App";
 
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 class App extends React.Component {
-  render() {
+  ReactDOM.render() {
     return (
       <div className="container">
         <div className="row">
