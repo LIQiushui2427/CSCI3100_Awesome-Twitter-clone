19948a7fe439183b50a2cf69d40ce6f7d3f31b05
CarlosCUHK <1155160208@link.cuhk.edu.hk>
2023-04-01 19:39:12 +0800

    tmp

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,19 +9,10 @@ import Home from '../components/home/home';
 class App extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
-      
+    
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
         <LeftPane />
           <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
-=======
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <LeftPane />
-          </div>
-          <div className="col-md-4">
->>>>>>> parent of d8e3213 (Home UI)
             <Home />
           </div>
           <div className="col-md-8">
