67f4d6bb497b42758f069088b01b8305d5b36131
CarlosCUHK <1155160208@link.cuhk.edu.hk>
2023-04-12 22:20:35 +0800

    update

--- client/components/EditableImage.js
+++ client/components/EditableImage.js
@@ -2,21 +2,23 @@
  * Borrow some part of the code from https://github.com/dejwid/twitter-clone/blob/master/components/EditableImage.js
  */
 
-import {useState} from "react";
-import {FileDrop} from "react-file-drop";
-import {PulseLoader} from "react-spinners";
-import {updateUser} from "../helper/helper";
-export default function EditableImage({type,src,onChange,className,editable=false}) {
-  const [isFileNearby, setIsFileNearby] = useState(false);
-  const [isFileOver,setIsFileOver] = useState(false);
-  const [isUploading,setIsUploading] = useState(false);
+import { useState } from "react";
+import { FileDrop } from "react-file-drop";
+import { PulseLoader } from "react-spinners";
+import { updateUser } from "../helper/helper";
+import convertToBase64 from '../helper/convert';
 
+export default function EditableImage({ type, src, onChange, className, editable = false }) {
+  const [isFileNearby, setIsFileNearby] = useState(false);
+  const [isFileOver, setIsFileOver] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+  const [coverbase, setCoverBase] = useState(false);
   let extraClasses = '';
   if (isFileNearby && !isFileOver) extraClasses += ' bg-blue-500 opacity-40';
   if (isFileOver) extraClasses += ' bg-blue-500 opacity-90';
   if (!editable) extraClasses = '';
 
-  function updateImage(files,e) {
+  async function updateImage(files, e) {
     if (!editable) {
       return;
     }
@@ -25,15 +27,16 @@ export default function EditableImage({type,src,onChange,className,editable=fals
     setIsFileOver(false);
     setIsUploading(true);
     const data = new FormData();
-    data.append(type, files[0]);
+    const base64 = await convertToBase64(files[0]);
+    setCoverBase(base64);
+    data.append(type, coverbase);
     const formDataObj = {}
-    for (let [key, value] of data.entries()){
+    for (let [key, value] of data.entries()) {
       formDataObj[key] = value;
     }
-    const formDataJSON = JSON.stringify(formDataObj)
-    updateUser(formDataJSON).then(async response => {
-      const json = await response.json();
-      onChange(json.src);
+    const formDataJSON = JSON.stringify(formDataObj);
+    updateUser(formDataJSON).then((response) => {
+      onChange(response.cover);
       setIsUploading(false);
     });
   }
@@ -51,15 +54,15 @@ export default function EditableImage({type,src,onChange,className,editable=fals
       }}
     >
       <div className={"bg-twitterBorder text-white relative"}>
-        <div className={'absolute inset-0 '+extraClasses}></div>
+        <div className={'absolute inset-0 ' + extraClasses}></div>
         {isUploading && (
           <div className="absolute inset-0 flex items-center justify-center"
-               style={{backgroundColor:'rgba(48, 140, 216,0.9)'}}>
+            style={{ backgroundColor: 'rgba(48, 140, 216,0.9)' }}>
             <PulseLoader size={14} color={'#fff'} />
           </div>
         )}
-        <div className={"cover flex items-center overflow-hidden "+className}>
-          {src && (<img src={src} className="w-full" alt=""/>)}
+        <div className={"cover flex items-center overflow-hidden " + className}>
+          {src && (<img src={src} className="w-full" alt="" />)}
         </div>
       </div>
     </FileDrop>
