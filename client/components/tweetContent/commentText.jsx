41772ac5666b3bb49ffc4d3d34afbaab64a8968d
LiQiushui <83109207+LIQiushui2427@users.noreply.github.com>
2023-05-05 16:15:34 +0800

    modified:   client/components/tweetContent/commentText.jsx

--- client/components/tweetContent/commentText.jsx
+++ client/components/tweetContent/commentText.jsx
@@ -79,7 +79,7 @@ function CommentText({ commentor = 'test_commentor', commentor_name = 'Tweeter',
         <Avatar src={avatarpic} alt="Test" />
         <div className="ml-4">
           <div className="flex items-center">
-            <span className="font-bold text-lg">{commentor_name}</span>
+            <span className="font-bold text-lg">{commentor}</span>
             <span className="ml-2 text-gray-500">@{commentor}</span>
             <span className="text-gray-500 mx-2">&middot;</span>
             <span className="text-gray-500">{time}</span>
