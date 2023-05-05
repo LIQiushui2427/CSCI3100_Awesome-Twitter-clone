77fdfa9b50677dcce7b2d7dc1cc4cb5acceda460
beeper <124332503+c-beeper@users.noreply.github.com>
2023-05-05 13:56:16 +0000

    Update 6 files

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,41 +1,62 @@
-import jwt from 'jsonwebtoken';
-import ENV from '../config.js';
-import User from '../model/User.model.js';
+/*
+    This file is for some useful functions of authenticating users.
+*/
+import jwt from "jsonwebtoken";
+import ENV from "../config.js";
+import User from "../model/User.model.js";
 
-export default async function Auth(req, res, next){
-    if (req.headers && req.headers.authorization){
-        const token = req.headers.authorization.split(' ')[1];
-        try{
-            const decode = jwt.verify(token,ENV.JWT_SECRET);
-            const user = await User.findById(decode.userId);
-            if(!user){
-                res.status(401).json({ error : "Authentication Failed!"})
-            }
-            req.user = user;
-            console.log(req.user._id);
-            next()
-        } catch(error){
-            res.status(401).json({ error : "Authentication Failed!"})
-        }
+/*
+    This function checks whether the user is logged in by verifying the token. If verified, the information
+    of this user will be stored in req.user for further usage.
+    (For the functions following this function, they can directly access req.user for user information.)
+*/
+export default async function Auth(req, res, next) {
+  //check whether the header contains authorization information
+  if (req.headers && req.headers.authorization) {
+    /*
+            Get the token.
+            The token, as jwt tokens, are sent in the header via Authorization: Bearer <token>
+        */
+    const token = req.headers.authorization.split(" ")[1];
+    try {
+      //decode the token and check whether this user exists in the database
+      const decode = jwt.verify(token, ENV.JWT_SECRET);
+      const user = await User.findById(decode.userId);
+      if (!user) {
+        //user does not exist
+        res.status(401).json({ error: "Authentication Failed!" });
+      }
+      //store the user information and perform the next steps
+      req.user = user;
+      console.log(req.user._id);
+      next();
+    } catch (error) {
+      res.status(401).json({ error: "Authentication Failed!" });
     }
-    else{
-        res.status(401).json({ error : "Authentication Failed!"})
-    }
+  } else {
+    res.status(401).json({ error: "Authentication Failed!" });
+  }
 }
 
-export async function checkAdmin(req, res, next){
-    if (req.user && req.user.isAdmin){
-        next();
-    }
-    else{
-        res.status(401).json({ error : "Authentication Failed!"})
-    }
+/*
+    This function checks whether the user is an admin user. It should be called after Auth().
+    Since user information is already stored, this function is rather straightforward.
+*/
+export async function checkAdmin(req, res, next) {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).json({ error: "Authentication Failed!" });
+  }
 }
 
-export function localVariables(req, res, next){
-    req.app.locals = {
-        OTP : null,
-        resetSession : false
-    }
-    next()
+/*
+    This function provides some local variables for sending OTP.
+*/
+export function localVariables(req, res, next) {
+  req.app.locals = {
+    OTP: null,
+    resetSession: false,
+  };
+  next();
 }
--- server/router/router.js
+++ server/router/router.js
@@ -1,23 +1,34 @@
-import {Router} from "express";
-import * as controller from '../controllers/appController.js';
-import Auth, {localVariables}  from "../middleware/auth.js";
+import { Router } from "express";
+import * as controller from "../controllers/appController.js";
+import Auth, { localVariables } from "../middleware/auth.js";
 
 const router = Router();
 
+/*POST request for /api/signup*/
+router.route("/signup").post(controller.signup);
+/*POST request for /api/login*/
+router.route("/login").post(controller.verifyUser, controller.login);
+/*POST request for /api/logout, login required*/
+router.route("/logout").post(Auth, controller.logout);
 
-/* POST request*/ 
-router.route('/signup').post(controller.signup);
-router.route('/login').post(controller.verifyUser, controller.login);
-router.route('/logout').post(Auth, controller.logout);
-
-/* GET request*/ 
-router.route('/user/:username').get(controller.getUser)//modified, any frontend used this should be changed
-router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP) 
-router.route('/verifyOTP').get(controller.verifyUser, controller.verifyOTP)
-router.route('/createResetSession').get(controller.createResetSession)
+/*GET request for /api/user/:username*/
+router.route("/user/:username").get(controller.getUser); //modified, any frontend used this should be changed
+/*GET request for /api/generateOTP*/
+router
+  .route("/generateOTP")
+  .get(controller.verifyUser, localVariables, controller.generateOTP);
+/*GET request for /api/verifyOTP*/
+router.route("/verifyOTP").get(controller.verifyUser, controller.verifyOTP);
+/*GET request for /api/createResetSession*/
+router.route("/createResetSession").get(controller.createResetSession);
+/*GET request for /api/searchUsers*/
 router.route("/searchUsers").get(controller.searchUsers);
-/* PUT request*/ 
-router.route('/resetPassword').put(controller.verifyUser, controller.resetPassword);
-router.route('/updateUser').put(Auth, controller.updateUser);
+
+/*PUT request for /api/resetPassword*/
+router
+  .route("/resetPassword")
+  .put(controller.verifyUser, controller.resetPassword);
+/*PUT request for /api/updateUser, login required*/
+router.route("/updateUser").put(Auth, controller.updateUser);
 
 export default router;
